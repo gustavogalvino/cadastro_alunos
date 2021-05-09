5184dcd41fb6dde1dafa2c3aeaf564fdb8ed005b
gustavogalvino <gustavodr09@gmail.com>
2021-05-09 17:17:01 -0300

    update

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from requests import api
+import os
 
 
 def busca_cep(cep):
@@ -81,4 +82,5 @@ def delete(ra):
 
 if __name__ == '__main__':
     db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host='0.0.0.0', port=port)
